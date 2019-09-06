44ec4ea25a98549df865987dc191c9b3230b687e
gabigaltieri <52494246+gabigaltieri@users.noreply.github.com>
2019-09-06 18:23:34 -0300

    Create Funciones.h

---
+++ Funciones.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    int Suma (int numero1, int numero2);
+    int Resta (int numero1, int numero2);
+    int Division (int numero1, int numero2);
+    int Multiplicacion(int numero1, int numero2);
+    int Factorial(int fact);
+ return 0;
+
+   }
+
+
